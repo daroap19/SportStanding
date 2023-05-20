0ae7af0002f05e81fca710d0b24a5b1a17bd13ea
JuanGaravito <juang12162002@gmail.com>
2023-05-20 14:21:23 -0500

    Tablas de Posiciones Soccer

--- main.jsx
+++ main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './src/App.jsx'
+import {App} from './src/App.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <App idLeague={"140"}/>
+    <App idLeague={"39"}/>
+    <App idLeague={"137"}/>
   </React.StrictMode>,
 )
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,64 @@
-export function App() {
+import { useEffect, useState } from "react"
+
+
+const requestOptions = {
+  method: 'GET',
+  headers: {
+    "x-rapidapi-key": "9084ce6eb502811b653b3d6e1e902035",
+    "x-rapidapi-host": "v3.football.api-sports.io"
+  },
+  redirect: 'follow'
+};
+
+const url = "https://v3.football.api-sports.io/"
+
+
+export function App({idLeague}) {
+  const [result, setResult] = useState(null)
+
+  useEffect(()=>{
+      fetch(url+`standings?league=${idLeague}&season=2022`, requestOptions)
+      .then(response => response.json())
+      .then(result => setResult(result.response[0].league.standings[0]))
+      .catch(error => console.log('error', error));
+  }, [])
+
 
   return (
-    <>
-     <h1>Inicio Proyecto Web</h1>
-    </>
+    <table>
+      <thead>
+        <tr>
+          <th>POS</th>
+          <th>EQUIPO</th>
+          <th>PT</th>
+          <th>PJ</th>
+          <th>PG</th>
+          <th>PP</th>
+          <th>PE</th>
+          <th>GA</th>
+          <th>GE</th>
+          <th>GDF</th>
+        </tr>
+      </thead>
+      <tbody>
+         {
+           result && result.map((res)=>(
+            <tr key={res.team.id}>
+              <td>{res.rank}</td>
+              <td>{res.team.name}</td>
+              <td>{res.points}</td>
+              <td>{res.all.played}</td>
+              <td>{res.all.win}</td>
+              <td>{res.all.lose}</td>
+              <td>{res.all.draw}</td>
+              <td>{res.all.goals.for}</td>
+              <td>{res.all.goals.against}</td>
+              <td>{res.all.goals.for - res.all.goals.against}</td>
+            </tr>
+           ))
+        }
+      </tbody>
+    </table>
   )
 }
 
